f6a791c3c86110a727385aaac84f0b70dd2d74e5
SanalThuruvelil <sanalsunny16@gmail.com>
2023-09-18 07:50:55 -0700

    Package edited

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'I - Twin Traders' });
+  res.render('about', { value : "This is about page" });
 });
 
 module.exports = router;
